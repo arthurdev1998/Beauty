88ebf70d8a2559e2cae511d47eb7f2db7360ceee
Arthur Mendes <arthur.mendes@fcxlabs.com.br>
2024-07-30 23:57:30 -0300

    tsunade

--- BeautySalon/Teste/ArquivoTeste.cs
+++ BeautySalon/Teste/ArquivoTeste.cs
@@ -5,5 +5,6 @@ namespace BeautySalon.Teste
         public string Naruto { get; set; }
         public string Sakura { get; set; }
         public string Jiraya { get; set; }
+        public string Tsunade { get; set; }
     }
 }
